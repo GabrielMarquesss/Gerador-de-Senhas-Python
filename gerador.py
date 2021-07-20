237a4b2e7a7c76a0c4d82c613e1f65732603c20c
Gabriel Marques <contato.gabrielmarques.jobs@gmail.com>
2021-07-20 09:20:52 -0400

    Atualização e comentários
    A outra versão apenas reescrevia a senha, não pulando a linha
    Resolvido este problema e alguns comentários no código

--- gerador.py
+++ gerador.py
@@ -5,10 +5,14 @@ from string import punctuation
 from string import ascii_letters
 from numbers import Number
 
+#Variável que pega todos os dígitos disponíveis das bibliotecas acima
 symbols = ascii_letters + punctuation + digits + str(Number)
+#Gerador de números aleatórios com base no OS
 secure_random = random.SystemRandom()
+#Criação da senha, para alterar o tamanho de caracteres da mesma, mude o número do range
 passwd = "".join(secure_random.choice(symbols)
                 for i in range(20))
-sys.stdout = open('output.txt', 'w')
-print(passwd)
-sys.stdout.close()
+#Cria/Abre e escreve no arquivo txt onde as senhas serão armazenadas
+with open('output.txt','a') as senhas:
+    senhas.write(passwd)
+    senhas.write('\n')
